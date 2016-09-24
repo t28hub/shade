e9f45af672f96b14bf47ddd8d54d49614ccff406
Tatsuya Maki <t28oogle@gmail.com>
2016-09-24 16:15:40 +0900

    Added a processor, annotations and an example model class

---
+++ shade/src/main/java/io/t28/shade/annotations/Shade.java
@@ -0,0 +1,21 @@
+package io.t28.shade.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+public @interface Shade {
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.CLASS)
+    @interface Preference {
+        String value();
+    }
+
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.CLASS)
+    @interface Property {
+        String value();
+    }
+}
