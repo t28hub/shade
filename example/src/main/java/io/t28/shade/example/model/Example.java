574d8fd5ec6a594c3154ff4577c954edfdf05e1b
Tatsuya Maki <t28oogle@gmail.com>
2016-09-25 14:55:49 +0900

    Added an entity loading from a SharedPreferences

--- example/src/main/java/io/t28/shade/example/model/Example.java
+++ example/src/main/java/io/t28/shade/example/model/Example.java
@@ -1,19 +1,15 @@
 package io.t28.shade.example.model;
 
 import io.t28.shade.annotations.Shade;
-import io.t28.shade.example.MainActivity;
 
 @Shade.Preference("io.t28.shade.example")
 public interface Example {
     @Shade.Property("int_value")
     int intValue();
 
-    @Shade.Property("long_value")
+    @Shade.Property(value = "long_value", defValue = "1024")
     long longValue();
 
     @Shade.Property("string_value")
     String stringValue();
-
-    @Shade.Property("hoge_value")
-    MainActivity hoge();
 }
