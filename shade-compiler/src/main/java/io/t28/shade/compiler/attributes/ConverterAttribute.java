12d78dd04cf01918c504ef409720fce8341a52d4
Tatsuya Maki <t28oogle@gmail.com>
2016-11-13 14:32:04 +0900

    Refactored ConverterAttribute and added unit tests for that

--- shade-compiler/src/main/java/io/t28/shade/compiler/attributes/ConverterAttribute.java
+++ shade-compiler/src/main/java/io/t28/shade/compiler/attributes/ConverterAttribute.java
@@ -1,6 +1,7 @@
 package io.t28.shade.compiler.attributes;
 
-import com.google.common.base.MoreObjects;
+import android.support.annotation.VisibleForTesting;
+
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.TypeName;
 
@@ -8,9 +9,7 @@ import java.util.List;
 
 import javax.annotation.Nonnull;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.MirroredTypeException;
 
-import io.t28.shade.annotations.Shade;
 import io.t28.shade.compiler.SupportedType;
 import io.t28.shade.compiler.utils.TypeNames;
 import io.t28.shade.converters.Converter;
@@ -26,45 +25,30 @@ public class ConverterAttribute {
     private final TypeName supportedType;
     private final TypeName convertedType;
 
-    private ConverterAttribute(@Nonnull ClassName className, @Nonnull TypeName supportedType, @Nonnull TypeName convertedType) {
-        this.className = className;
-        this.supportedType = supportedType;
-        this.convertedType = convertedType;
+    @SuppressWarnings("WeakerAccess")
+    public ConverterAttribute(@Nonnull Class<?> type) {
+        this(ClassName.get(type), TypeNames.collectGenericsTypes(type, Converter.class));
     }
 
-    @Nonnull
-    static ConverterAttribute create(@Nonnull Shade.Property annotation) {
-        ClassName className;
-        List<TypeName> types;
-        try {
-            final Class<?> converterClass = annotation.converter();
-            className = ClassName.get(converterClass);
-            types = TypeNames.collectGenericsTypes(converterClass, Converter.class);
-        } catch (MirroredTypeException e) {
-            final TypeElement element = TypeNames.toTypeElement(e.getTypeMirror());
-            className = ClassName.get(element);
-            types = TypeNames.collectGenericsTypes(element, Converter.class);
-        }
+    @SuppressWarnings("WeakerAccess")
+    public ConverterAttribute(@Nonnull TypeElement element) {
+        this(ClassName.get(element), TypeNames.collectGenericsTypes(element, Converter.class));
+    }
 
+    @VisibleForTesting
+    ConverterAttribute(@Nonnull ClassName className, @Nonnull List<TypeName> types) {
         if (types.size() != GENERICS_SIZE) {
-            throw new IllegalArgumentException("Specified converter(" + className + ") does not have enough generics types");
+            throw new IllegalArgumentException("Specified converter(" + className + ") does not have enough generic types");
         }
 
         final TypeName supportedType = TypeNames.unbox(types.get(INDEX_SUPPORTED_TYPE));
         final TypeName convertedType = TypeNames.unbox(types.get(INDEX_CONVERTED_TYPE));
         if (!DEFAULT_CLASS.equals(className) && !SupportedType.contains(supportedType)) {
-            throw new IllegalArgumentException("Cannot save returnType(" + supportedType + ") to a SharedPreferences");
+            throw new IllegalArgumentException("SharedPreferences does not support to save type(" + supportedType + ")");
         }
-        return new ConverterAttribute(className, supportedType, convertedType);
-    }
-
-    @Override
-    public String toString() {
-        return MoreObjects.toStringHelper(this)
-                .add("className", className)
-                .add("supportedType", supportedType)
-                .add("convertedType", convertedType)
-                .toString();
+        this.className = className;
+        this.supportedType = supportedType;
+        this.convertedType = convertedType;
     }
 
     public boolean isDefault() {
