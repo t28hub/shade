f57d6e9c4f0cc18faac5ab8d16a9174915ad15bf
Tatsuya Maki <t28oogle@gmail.com>
2016-11-22 22:06:46 +0900

    Added annotations to Converter and added some converter implementations

---
+++ shade/src/main/java/io/t28/shade/converters/UriConverter.java
@@ -0,0 +1,27 @@
+package io.t28.shade.converters;
+
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class UriConverter implements Converter<Uri, String> {
+    private static final String EMPTY_URI = "";
+
+    @NonNull
+    @Override
+    public Uri toConverted(@Nullable String supported) {
+        if (supported == null) {
+            return Uri.EMPTY;
+        }
+        return Uri.parse(supported);
+    }
+
+    @NonNull
+    @Override
+    public String toSupported(@Nullable Uri converted) {
+        if (converted == null) {
+            return EMPTY_URI;
+        }
+        return converted.toString();
+    }
+}
