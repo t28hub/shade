591579266a797aa06a08981c03e0da42ff15cc98
Tatsuya Maki <t28oogle@gmail.com>
2016-11-22 21:17:22 +0900

    Improved code quality (#57)

--- shade-compiler/src/main/java/io/t28/shade/compiler/factories/editor/UnchangedBitConstantFactory.java
+++ shade-compiler/src/main/java/io/t28/shade/compiler/factories/editor/UnchangedBitConstantFactory.java
@@ -3,7 +3,6 @@ package io.t28.shade.compiler.factories.editor;
 import com.squareup.javapoet.FieldSpec;
 
 import javax.annotation.Nonnull;
-import javax.inject.Inject;
 import javax.lang.model.element.Modifier;
 
 import io.t28.shade.compiler.factories.FieldFactory;
@@ -11,10 +10,6 @@ import io.t28.shade.compiler.factories.FieldFactory;
 public class UnchangedBitConstantFactory extends FieldFactory {
     private static final String CONSTANT_UNCHANGED = "UNCHANGED";
     private static final String INITIAL_UNCHANGED = "0x0L";
-
-    @Inject
-    public UnchangedBitConstantFactory() {
-    }
 
     @Nonnull
     @Override
