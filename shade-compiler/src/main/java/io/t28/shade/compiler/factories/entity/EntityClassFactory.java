b07f08b7f2a31efa9cfcaaae5f6fa67333c66d6a
Tatsuya Maki <t28oogle@gmail.com>
2016-11-23 23:33:56 +0900

    Improved entity class building process

--- shade-compiler/src/main/java/io/t28/shade/compiler/factories/entity/EntityClassFactory.java
+++ shade-compiler/src/main/java/io/t28/shade/compiler/factories/entity/EntityClassFactory.java
@@ -2,45 +2,50 @@ package io.t28.shade.compiler.factories.entity;
 
 import com.google.common.collect.ImmutableList;
 import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.CodeBlock;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeName;
-import com.squareup.javapoet.TypeSpec;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
 
-import io.t28.shade.compiler.factories.Factory;
-import io.t28.shade.compiler.factories.FieldFactory;
-import io.t28.shade.compiler.factories.MethodFactory;
+import io.t28.shade.compiler.attributes.PropertyAttribute;
 import io.t28.shade.compiler.factories.TypeFactory;
 
 import static java.util.stream.Collectors.toList;
 
 public class EntityClassFactory extends TypeFactory {
+    private static final ClassName CLASS_LIST = ClassName.get(List.class);
+    private static final ClassName CLASS_SET = ClassName.get(Set.class);
+    private static final ClassName CLASS_MAP = ClassName.get(Map.class);
+
     private final TypeElement element;
-    private final List<FieldFactory> fieldFactories;
-    private final List<MethodFactory> methodFactories;
+    private final List<PropertyAttribute> properties;
     private final ClassName entityClass;
     private final ClassName entityImplClass;
 
     @Inject
     public EntityClassFactory(@Nonnull TypeElement element,
-                              @Nonnull @Named("Entity") List<FieldFactory> fieldFactories,
-                              @Nonnull @Named("Entity") List<MethodFactory> methodFactories,
+                              @Nonnull List<PropertyAttribute> properties,
                               @Nonnull @Named("Entity") ClassName entityClass,
                               @Nonnull @Named("EntityImpl") ClassName entityImplClass) {
         this.element = element;
-        this.fieldFactories = ImmutableList.copyOf(fieldFactories);
-        this.methodFactories = ImmutableList.copyOf(methodFactories);
+        this.properties = ImmutableList.copyOf(properties);
         this.entityClass = entityClass;
         this.entityImplClass = entityImplClass;
     }
@@ -78,16 +83,86 @@ public class EntityClassFactory extends TypeFactory {
     @Nonnull
     @Override
     protected List<FieldSpec> fields() {
-        return fieldFactories.stream()
-                .map(Factory::create)
+        final List<FieldSpec> fieldSpecs = properties
+                .stream()
+                .map(property -> FieldSpec.builder(property.returnTypeName(), property.methodName())
+                        .addModifiers(Modifier.PRIVATE, Modifier.FINAL)
+                        .build())
                 .collect(toList());
+        return ImmutableList.copyOf(fieldSpecs);
     }
 
     @Nonnull
     @Override
     protected List<MethodSpec> methods() {
-        return methodFactories.stream()
-                .map(Factory::create)
+        return ImmutableList.<MethodSpec>builder()
+                .add(buildConstructorSpec())
+                .addAll(buildGetMethodSpecs())
+                .build();
+    }
+
+    private MethodSpec buildConstructorSpec() {
+        final MethodSpec.Builder builder = MethodSpec.constructorBuilder();
+        builder.addModifiers(Modifier.PRIVATE);
+
+        // Parameters
+        properties.forEach(property -> {
+            final ParameterSpec parameter = ParameterSpec.builder(property.returnTypeName(), property.methodName())
+                    .addModifiers(Modifier.FINAL)
+                    .build();
+            builder.addParameter(parameter);
+        });
+
+        // Statements
+        properties.forEach(property -> {
+            final CodeBlock statement = CodeBlock.builder()
+                    .add("this.$L = $L", property.methodName(), createUnmodifiableStatement(property.returnType(), property.methodName()))
+                    .build();
+            builder.addStatement("$L", statement);
+        });
+        return builder.build();
+    }
+
+    private List<MethodSpec> buildGetMethodSpecs() {
+        return properties.stream()
+                .map(property -> {
+                    final CodeBlock statement = createUnmodifiableStatement(property.method(), property.methodName());
+                    return MethodSpec.overriding(property.method())
+                            .addModifiers(Modifier.FINAL)
+                            .addStatement("return $L", statement)
+                            .build();
+                })
                 .collect(toList());
+    }
+
+    @Nonnull
+    protected CodeBlock createUnmodifiableStatement(@Nonnull ExecutableElement method, @Nonnull String name) {
+        return createUnmodifiableStatement(method.getReturnType(), name);
+    }
+
+    @Nonnull
+    private CodeBlock createUnmodifiableStatement(@Nonnull TypeMirror typeMirror, @Nonnull String name) {
+        final TypeName typeName = TypeName.get(typeMirror);
+        if (typeName instanceof ParameterizedTypeName) {
+            return createUnmodifiableCollectionStatement((ParameterizedTypeName) typeName, name);
+        }
+        return CodeBlock.of("$N", name);
+    }
+
+    @Nonnull
+    private CodeBlock createUnmodifiableCollectionStatement(@Nonnull ParameterizedTypeName typeName, @Nonnull String name) {
+        final TypeName rawType = typeName.rawType;
+        if (rawType.equals(CLASS_LIST)) {
+            return CodeBlock.of("$T.unmodifiableList($N)", Collections.class, name);
+        }
+
+        if (rawType.equals(CLASS_SET)) {
+            return CodeBlock.of("$T.unmodifiableSet($N)", Collections.class, name);
+        }
+
+        if (rawType.equals(CLASS_MAP)) {
+            return CodeBlock.of("$T.unmodifiableMap($N)", Collections.class, name);
+        }
+        return CodeBlock.of("$N", name);
     }
 }
--- shade-compiler/src/main/java/io/t28/shade/compiler/inject/entity/EntityModule.java
+++ shade-compiler/src/main/java/io/t28/shade/compiler/inject/entity/EntityModule.java
@@ -3,38 +3,22 @@ package io.t28.shade.compiler.inject.entity;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.Provides;
-import com.google.inject.TypeLiteral;
 import com.google.inject.name.Names;
 import com.squareup.javapoet.ClassName;
 
-import java.util.List;
-
 import javax.annotation.Nonnull;
 import javax.inject.Named;
-import javax.inject.Singleton;
 import javax.lang.model.element.TypeElement;
 
-import io.t28.shade.compiler.factories.FieldFactory;
-import io.t28.shade.compiler.factories.MethodFactory;
 import io.t28.shade.compiler.factories.TypeFactory;
 import io.t28.shade.compiler.factories.entity.EntityClassFactory;
 
 @SuppressWarnings("unused")
 public class EntityModule implements Module {
-    private static final String ENTITY_IMPL_SUFFIX = "Impl";
+    private static final String ENTITY_IMPL_SUFFIX = "$$Impl";
 
     @Override
     public void configure(Binder binder) {
-        binder.bind(new TypeLiteral<List<FieldFactory>>(){})
-                .annotatedWith(Names.named("Entity"))
-                .toProvider(FieldListProvider.class)
-                .in(Singleton.class);
-
-        binder.bind(new TypeLiteral<List<MethodFactory>>(){})
-                .annotatedWith(Names.named("Entity"))
-                .toProvider(MethodListProvider.class)
-                .in(Singleton.class);
-
         binder.bind(TypeFactory.class)
                 .annotatedWith(Names.named("Entity"))
                 .to(EntityClassFactory.class);
